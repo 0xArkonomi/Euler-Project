5226bf77fd5293af5a2b2056714b8462ce547790
Mohammad Mahdi Keshavarz <arkonomii@gmail.com>
2023-10-03 01:03:00 +0330

    Add more straightforward euler_02.js solution

--- euler_02.js
+++ euler_02.js
@@ -31,3 +31,28 @@ for (let i = 0; i < fibonacciArray.length; i++) {
 console.log(`The sum of even Fibonacci numbers not exceeding ${limit.toLocaleString()} is ${sum.toLocaleString()}`);
 // console.log('Fibonacci sequence:', fibonacciArray);
 
+
+// ------------- ↓ More straightforward solution ↓ -------------
+
+// Function to calculate the sum of even Fibonacci numbers not exceeding the given 'limit'.
+function evenFibonacciSum(limit) {
+    let sum = 0;
+    let previous = 1;        // Initialize the first Fibonacci number to 1.
+    let current = 2;        // Initialize the second Fibonacci number to 2.
+  
+    while (previous <= limit) {  // Continue while the current Fibonacci number is less than or equal to the 'limit'.
+      if (previous % 2 === 0) {  // Check if the current Fibonacci number is even.
+        sum += previous;         // If even, add it to the sum.
+      }
+      [previous, current] = [current, previous + current]; // Use array destructuring to update 'previous' and 'current' with the next Fibonacci numbers.
+    }
+  
+    return sum;  // Return the sum of even Fibonacci numbers.
+  }
+  
+//   const limit = 4_000_000;
+  const result = evenFibonacciSum(limit); 
+  
+  // Output the result with proper formatting using 'toLocaleString()' for better readability.
+  console.log(`The sum of even Fibonacci numbers not exceeding ${limit.toLocaleString()} is ${result.toLocaleString()}`);
+  

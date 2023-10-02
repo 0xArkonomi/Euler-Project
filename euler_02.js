b7e2b8f02707edb9a41ef61653d8282a419bd2c4
Mohammad Mahdi Keshavarz <arkonomii@gmail.com>
2023-10-03 01:01:56 +0330

    Add euler_02.js solution

--- euler_02.js
+++ euler_02.js
@@ -1,34 +1,33 @@
-//
-let start = 0;
-
-//
-const end = 4000000; 
-
-//
-let sum = 0;
-
-function setFibonacci(endOfSequence) {
-    let array = [];
-    let sum = 0;
+// Function to generate a Fibonacci sequence up to the specified limit
+function generateFibonacci(endOfSequence) {
+    const fibonacciArray = [0, 1];
+    let previous = 0;
     let current = 1;
-    let previous;
-    for (let i = 1; i < end; i++) {
-        previous = sum;
-        sum += current;
-        array.push(sum);
-        current = previous;
+
+    while (current + previous <= endOfSequence) {
+        const next = current + previous;
+        fibonacciArray.push(next);
+        previous = current;
+        current = next;
     }
-    return array;
-}   
 
-let array = setFibonacci(end);
-
-//
-while (array[start] < end) {
-    if (array[start] % 2 == 0)
-        sum += start;
-    start++;
+    return fibonacciArray;
 }
 
-console.log(sum);
-console.log(array);
+const limit = 4000000; // Define the limit as 4,000,000 .
+let sum = 0; // Initialize sum with zero
+
+// Call the function to generate the Fibonacci sequence
+const fibonacciArray = generateFibonacci(limit);
+
+// Calculate the sum of even Fibonacci numbers
+for (let i = 0; i < fibonacciArray.length; i++) {
+    if (fibonacciArray[i] % 2 === 0) {
+        sum += fibonacciArray[i];
+    }
+}
+
+// Output the sum and the Fibonacci sequence
+console.log(`The sum of even Fibonacci numbers not exceeding ${limit.toLocaleString()} is ${sum.toLocaleString()}`);
+// console.log('Fibonacci sequence:', fibonacciArray);
+

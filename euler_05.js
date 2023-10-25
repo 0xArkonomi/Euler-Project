d8bf416efd4251ca3dc7c8bda93a0043725513a2
Mohammad Mahdi Keshavarz <arkonomii@gmail.com>
2023-10-25 23:28:39 +0330

    Add basic sulotion to euler_05.js

---
+++ euler_05.js
@@ -0,0 +1,16 @@
+let number = 1;
+while (
+    number % 1 != 0 &&
+    number % 2 != 0 &&
+    number % 3 != 0 &&
+    number % 4 != 0 &&
+    number % 5 != 0 &&
+    number % 6 != 0 &&
+    number % 7 != 0 &&
+    number % 8 != 0 &&
+    number % 9 != 0 &&
+    number % 10 != 0
+) {
+    number = number + 1;
+}
+console.log(number);
